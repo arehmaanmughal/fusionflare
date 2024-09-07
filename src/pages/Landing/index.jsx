17b1415fd99ccfb2888875b4a8cd1c7b0d4cc76a
Abdul Rehman <102214032+arehmaanmughal@users.noreply.github.com>
2024-09-07 23:33:58 +0500

    update landing page

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import HeroSection from '../../components/HeroSection';
-import Mission from '../../components/Mission';
-import Deliver from '../../components/deliver';
-import Newsletter from '../../components/Newsletter';
-
+import React from "react";
+import HeroSection from "../../components/HeroSection";
+import Mission from "../../components/Mission";
+import Deliver from "../../components/Deliver";
+import Newsletter from "../../components/Newsletter";
 
 const LandingPage = () => {
   return (
     <div>
-     
       <HeroSection />
       <Mission />
       <Deliver />
       <Newsletter />
-     
+      
     </div>
-  )
-}
+  );
+};
 
 export default LandingPage;
